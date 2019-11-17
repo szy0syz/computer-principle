86b6218ee304d5190a544e1cd49436f16e120c03
szy0syz <szy0syz@gmail.com>
2019-11-17 09:49:27 +0800

    Finish 完成LRU缓存算法

--- LRUCache.js
+++ LRUCache.js
@@ -18,4 +18,43 @@ class LRUCache {
       return -1;
     }
   }
+
+  put(key, value) {
+    //* 如果节点在缓存中
+    if (key in this.map) {
+      const node = this.map.get(key);
+      this.list.remove(node);
+      node.value = value;
+      this.list.unshift(node);
+    } else {
+      const node = new Node(key, value);
+      //* 先判断链表size是否已经满了
+      if (this.list.size >= this.list.capacity) {
+        const tail_node = this.list.remove();
+        delete this.map[tail_node.key];
+      }
+      this.list.unshift(node);
+      this.map[key] = node;
+    }
+  }
+
+  print() {
+    this.list.print();
+  }
 }
+
+// ~~~test~~~
+const cache = new LRUCache(2);
+cache.put(2,2);
+cache.print();
+cache.put(1,1);
+cache.print();
+cache.put(3,3);
+cache.print();
+console.log(cache.get(1));
+cache.print();
+console.log(cache.get(2));
+cache.print();
+console.log(cache.get(3));
+cache.print();
+
